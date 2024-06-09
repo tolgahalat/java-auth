3685c3e804548d95ee4f665d09735f61ea9befd7
Tolga Halat <gtolgahalat@gmail.com>
2024-06-10 01:25:19 +0300

    first commit

---
+++ src/main/java/tolgstoy/javaauth/JavaAuthApplication.java
@@ -0,0 +1,13 @@
+package tolgstoy.javaauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAuthApplication.class, args);
+    }
+
+}
---
+++ src/main/java/tolgstoy/javaauth/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package tolgstoy.javaauth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    // User Creation
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+
+        // InMemoryUserDetailsManager
+        UserDetails admin = User.withUsername("Amiya")
+                .password(encoder.encode("123"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        UserDetails user = User.withUsername("Ejaz")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    // Configuring HttpSecurity
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/welcome").permitAll()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/auth/user/**").authenticated()
+                .and()
+                .authorizeHttpRequests().requestMatchers("/auth/admin/**").authenticated()
+                .and().formLogin()
+                .and().build();
+    }
+
+    // Password Encoding
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
